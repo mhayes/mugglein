566ce20dd486e8b1b7c5e0e67180779b0dbc1c1c
Mark Hayes <mark@zurb.com>
2012-06-08 23:14:18 -0700

    setup testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,39 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+
+class IntegrationTest < ActiveSupport::TestCase
+  require 'capybara/poltergeist'
+  include Rails.application.routes.url_helpers
+  include Capybara::DSL
+  
+  # Capybara.current_driver = :poltergeist
+  teardown do
+    ActionMailer::Base.deliveries = []
+  end
+  
+  # Allows :js => true on describe, but only for RSpec apparently
+  # Capybara.javascript_driver = :poltergeist
+  
+  # def login_as(user)
+  #   OmniAuth.config.add_mock :twitter, {
+  #     provider: 'twitter',
+  #     uid: user.twitter_uid,
+  #     info: {
+  #       nickname: user.twitter_nickname,
+  #       name: user.twitter_name,
+  #       location: user.twitter_location,
+  #       image: user.twitter_image
+  #     },
+  #     credentials: {
+  #       auth_token: "1234567890"
+  #     }
+  #   }
+  #   visit "/auth/twitter"
+  # end
+  
+end
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
