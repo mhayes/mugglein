a80d34326a65890f39b93ef788f9cff26a2720c6
Mark Hayes <mark@zurb.com>
2012-06-08 23:05:24 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mugglein::Application.config.secret_token = 'a2ff4820c19b55d221a85280bdd9539fa3ae8086da6047949039f45d924c0f9da166adc44d728e25f3ea55372776155ad0d4dab1458fb10ec64f38c17d607e3f'
